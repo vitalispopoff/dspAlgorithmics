7fe0d84a02657f9995609a1a3ba34733275b96cf
vitalispopoff <vitalis.popoff@gmail.com>
2020-08-29 11:13:19 +0200

    initial for gui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,46 @@
 import algorithms.metaProcessors.FileManager;
 import data.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) /*throws Exception*/ {
+
+        String
+                title = "Hello World";
+
+        Button
+                button = new Button();
+
+        button.setText(title);
+        button.setOnAction(event -> System.out.println(title));
+
+        StackPane
+                root = new StackPane();
+
+        root.getChildren().add(button);
+
+        int
+                width = 300,
+                height = 275;
+
+        Scene
+                scene = new Scene(root, width, height);
+
+        primaryStage.setTitle(title);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 
     public static void main(String[] args) {
+
+        launch(args);
 
         Wave
                 temporal,

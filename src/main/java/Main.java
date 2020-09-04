2ee945da05082423094f439c0df7b8963dd49cc6
vitalispopoff <vitalis.popoff@gmail.com>
2020-09-04 17:12:29 +0200

    Main and Grid clean

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,10 @@ import gui.Grid;
 import gui.MainMenu;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.*;
-
-import javafx.scene.effect.BlendMode;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.*;
-
-import java.util.ArrayList;
-
-import static javafx.scene.paint.Color.*;
 
 public class Main extends Application {
 
@@ -42,169 +31,8 @@ public class Main extends Application {
 
         stage.setScene(scene);
 
-/*        int
-            initHeightParam = (int) root.getHeight() - 50,
-            initWidthParam = (int) root.getWidth() - 20;
-
-        Rectangle
-            borderTop = new Rectangle(0, 0, initWidthParam, initHeightParam >> 5),
-            block_0 = new Rectangle(0, 0, initWidthParam, (initHeightParam >> 2) - 1),
-            block_1 = new Rectangle(0, 0, initWidthParam, (initHeightParam >> 3) - 1),
-            block_2 = new Rectangle(0, 0, initWidthParam, (initHeightParam >> 4) - 1),
-            block_3 = new Rectangle(0, 0, initWidthParam, (initHeightParam >> 5) - 1),
-            block_3last = new Rectangle(0, 0, initWidthParam, (initHeightParam >> 5) - 1),
-            block_2last = new Rectangle(0, 0, initWidthParam, (initHeightParam >> 4) - 1),
-            block_1last = new Rectangle(0, 0, initWidthParam, (initHeightParam >> 3) - 1),
-            block_last = new Rectangle(0, 0, initWidthParam, (initHeightParam >> 2) - 1),
-            borderBottom = new Rectangle(0, 0, initWidthParam, initHeightParam >> 5);
-
-        Line
-            full = new Line(0, 0, initWidthParam, 0),
-            half = new Line(0, 0, initWidthParam, 0),
-            quarter = new Line(0, 0, initWidthParam, 0),
-            eighth = new Line(0, 0, initWidthParam, 0),
-            center = new Line(0, 0, initWidthParam, 0),
-            _eighth = new Line(0, 0, initWidthParam, 0),
-            _quarter = new Line(0, 0, initWidthParam, 0),
-            _half = new Line(0, 0, initWidthParam, 0),
-            _full = new Line(0, 0, initWidthParam, 0);
-
-        VBox
-            grid = new VBox(
-            borderTop,
-            full, block_0,
-            half, block_1,
-            quarter, block_2,
-            eighth, block_3,
-            center,
-            block_3last, _eighth,
-            block_2last, _quarter,
-            block_1last, _half,
-            block_last, _full,
-            borderBottom
-        );
-
-        Color
-            color = Color.LIGHTBLUE;
-
-        Background
-            bcg = new Background(new BackgroundFill(GRAY, CornerRadii.EMPTY, Insets.EMPTY));
-
-        grid.setBackground(bcg);
-
-        StackPane
-            left = new StackPane(),
-            rite = new StackPane(),
-            bottom = new StackPane();
-
-        left.setBackground(bcg);
-        rite.setBackground(bcg);
-        bottom.setBackground(bcg);
-        left.setPrefWidth(10);
-        rite.setPrefWidth(10);
-        bottom.setMinHeight(25);
-
-        root.setLeft(left);
-        root.setRight(rite);
-        root.setBottom(bottom);
-
-
-
-        {
-
-            borderTop.setOpacity(0);
-            block_0.setOpacity(0);
-            block_1.setOpacity(0);
-            block_2.setOpacity(0);
-            block_3.setOpacity(0);
-            block_3last.setOpacity(0);
-            block_2last.setOpacity(0);
-            block_1last.setOpacity(0);
-            block_last.setOpacity(0);
-            borderBottom.setOpacity(0);
-
-            center.setStroke(color);
-            center.setStrokeWidth(1);
-            half.setStroke(color);
-            half.setStrokeWidth(1);
-            _half.setStroke(color);
-            _half.setStrokeWidth(1);
-            quarter.setStroke(color);
-            quarter.setStrokeWidth(1);
-            _quarter.setStroke(color);
-            _quarter.setStrokeWidth(1);
-            eighth.setStroke(color);
-            eighth.setStrokeWidth(1);
-            _eighth.setStroke(color);
-            _eighth.setStrokeWidth(1);
-            full.setStroke(color);
-            full.setStrokeWidth(1);
-            _full.setStroke(color);
-            full.setStrokeWidth(1);
-
-        }   // * setting colors & opacity
-
-
-
-
-        root.widthProperty().addListener((observable, oldValue, newValue) -> {
-
-            double
-                parameter = ((double)newValue) - 20;
-
-            grid.setPrefWidth(parameter);
-
-            block_0.setWidth(parameter);
-            block_last.setWidth(parameter);
-            block_1.setWidth(parameter);
-            block_1last.setWidth(parameter);
-            block_2.setWidth(parameter);
-            block_2last.setWidth(parameter);
-            block_3.setWidth(parameter);
-            block_3last.setWidth(parameter);
-
-            center.setEndX(parameter);
-            half.setEndX(parameter);
-            _half.setEndX(parameter);
-            quarter.setEndX(parameter);
-            _quarter.setEndX(parameter);
-            eighth.setEndX(parameter);
-            _eighth.setEndX(parameter);
-            full.setEndX(parameter);
-            _full.setEndX(parameter);
-        });
-
-        root.heightProperty().addListener((observable, oldValue, newValue) -> {
-
-            int
-                barHeight = (int) bar.getHeight(),
-                parameter = (int) (double)newValue - 50 ;
-
-            if (block_0.getHeight() != (parameter >>> 2) - 1.){
-
-                borderTop.setHeight(parameter >> 5);
-                borderBottom.setHeight(parameter >> 5);
-
-                block_0.setHeight((parameter >>> 2) - 1.);
-                block_last.setHeight((parameter >>> 2) - 1.);
-
-                block_1.setHeight((parameter >>> 3) - 1.);
-                block_1last.setHeight((parameter >>> 3) - 1.);
-
-                block_2.setHeight((parameter >>> 4) - 1.);
-                block_2last.setHeight((parameter >>> 4) - 1.);
-
-                block_3.setHeight((parameter >>> 5) - 1.);
-                block_3last.setHeight((parameter >>> 5) - 1.);
-            }
-        });
-
-        root.setCenter(grid);*/     // * moved to Grid class
-
         Grid
             grid = new Grid(root);
-
-//        root.setCenter(grid);
 
     //  ---------------------------------------------------------------------------------------
 
--- src/main/java/gui/Grid.java
+++ src/main/java/gui/Grid.java
@@ -18,120 +18,33 @@ public class Grid extends VBox{
 	int
 		elementIndex = 18;
 
+//	-------------------------------------------------------------------------------------------
+
 	public Grid(BorderPane root) {
 
 		setBackground(new Background(new BackgroundFill(GRAY, CornerRadii.EMPTY, Insets.EMPTY)));
 
 		populateBorders(root);
 		populateGrid(root);
-
 		observeWindow(root);
-
-		/*		int
-			initHeightParam = (int) root.getHeight() - 50,
-			initWidthParam = (int) root.getWidth() - 20;
-
-	//	grid elements //---------------------------------------------------------------------------------------
-
-		ArrayList<Shape>
-			elements = new ArrayList<>();
-
-		for (int i = 1; i < elementIndex ; i++) {
-
-			if (i % 2 == 0) {
-
-				int
-					shift = 1 + (Math.min(i, elementIndex - i) >>> 1);
-
-				Rectangle
-					r = new Rectangle(0, 0, initWidthParam, (initHeightParam >> shift) - 1);
-
-				r.setOpacity(0);
-				elements.add(r);
-			}
-
-			else {
-
-				Line
-					l = new Line(0, 0, initWidthParam, 0);
-
-				l.setStrokeWidth(1);
-				l.setStroke(color);
-				elements.add(l);
-			}
-		}
-
-		Rectangle
-			topBlock = new Rectangle(0, 0, initWidthParam, initHeightParam >>> 5),
-			bottomBlock = new Rectangle(0, 0, initWidthParam, initHeightParam >>> 5);
-
-		topBlock.setOpacity(0);
-		bottomBlock.setOpacity(0);
-
-		elements.add(0, topBlock);
-		elements.add(bottomBlock);
-
-		getChildren().addAll(elements);*/ // ! moved to a private method - disposable
-
-		/*        root.widthProperty().addListener((observable, oldValue, newValue) -> {
-
-			double
-				parameter = ((double)newValue) - 20;
-
-			setPrefWidth(parameter);
-
-			if (((Rectangle) getChildren().get(0)).getWidth() != parameter)
-
-				for (Node element : getChildren()) {
-
-					if (element instanceof Rectangle) ((Rectangle) element).setWidth(parameter);
-
-					else ((Line) element).setEndX(parameter);
-				}
-		});
-
-        root.heightProperty().addListener((observable, oldValue, newValue) -> {
-
-			int
-				barHeight = (int) ((MainMenu)root.getTop()).getHeight(),
-				parameter = (int) (double)newValue - 50 ,
-				numberOfElements = getChildren().size();
-
-			if (((Rectangle) getChildren().get(0)).getWidth() != parameter)
-
-				for (int i = 0; i < numberOfElements; i+= 2){
-
-					int
-						shifter = (i == 0 || i == numberOfElements - 1)
-							? 5
-							: 1 + (Math.min(i, numberOfElements - i) >>> 1);
-
-					((Rectangle) getChildren().get(i)).setHeight((parameter >>> shifter) - 1);
-				}
-		});*/	// ! moved to a private method - disposable
-
 		root.setCenter(this);
 	}
 
-
+//	-------------------------------------------------------------------------------------------
 
 	private void populateBorders(BorderPane root){
 
-		StackPane
-			left = new StackPane(),
-			rite = new StackPane(),
-			bottom = new StackPane();
+		StackPane[] borders = new StackPane[2];
 
-		left.setBackground(getBackground());
-		rite.setBackground(getBackground());
-		bottom.setBackground(getBackground());
-		left.setPrefWidth(10);
-		rite.setPrefWidth(10);
-		bottom.setMinHeight(25);
+		for (int i = 0; i < 2; i++){
 
-		root.setLeft(left);
-		root.setRight(rite);
-		root.setBottom(bottom);
+			borders[i] = new StackPane();
+			borders[i].setBackground(getBackground());
+			borders[i].setPrefWidth(10);
+		}
+
+		root.setLeft(borders[0]);
+		root.setRight(borders[1]);
 	}
 
 	private void populateGrid(BorderPane root){
@@ -147,19 +60,11 @@ public class Grid extends VBox{
 
 			if (i % 2 == 0) {
 
-/*				int
-					shifter = (i == 0 || i == elementIndex - 1)
-								  ? 5
-								  : 1 + (Math.min(i, elementIndex - i) >>> 1);*/	// * disposable
-
-/*				int
-					shifter = getShifter(i, elementIndex);*/	// * disposable
-
 				double
 					h = calculateHeight(initHeightParam, i, elementIndex);
 
 				Rectangle
-					r = new Rectangle(0, 0, initWidthParam, /*(initHeightParam >> shifter) - 1*/h);
+					r = new Rectangle(0, 0, initWidthParam, h);
 
 				r.setOpacity(0);
 				elements.add(r);
@@ -175,16 +80,6 @@ public class Grid extends VBox{
 				elements.add(l);
 			}
 		}
-
-	/*		Rectangle
-			topBlock = new Rectangle(0, 0, initWidthParam, initHeightParam >>> 5),
-			bottomBlock = new Rectangle(0, 0, initWidthParam, initHeightParam >>> 5);
-
-		topBlock.setOpacity(0);
-		bottomBlock.setOpacity(0);
-
-		elements.add(0, topBlock);
-		elements.add(bottomBlock);*/	// * top and bottom blocks - disposable
 
 		getChildren().addAll(elements);
 	}
@@ -211,7 +106,6 @@ public class Grid extends VBox{
 		root.heightProperty().addListener((observable, oldValue, newValue) -> {
 
 			int
-//				barHeight = (int) ((MainMenu)root.getTop()).getHeight(),
 				parameter = (int) (double)newValue - 50 ,
 				numberOfElements = getChildren().size();
 
@@ -219,18 +113,10 @@ public class Grid extends VBox{
 
 				for (int i = 0; i < numberOfElements; i+= 2){
 
-	/*					int
-						shifter = (i == 0 || i == numberOfElements - 1)
-									  ? 5
-									  : 1 + (Math.min(i, numberOfElements - i) >>> 1);*/	// * disposable
-
-	/*					int
-							shifter = getShifter(i, numberOfElements);*/	// * disposable
-
 					double
 						h = calculateHeight(parameter, i, numberOfElements);
 
-					((Rectangle) getChildren().get(i)).setHeight(/*(parameter >>> shifter) - 1*/h );
+					((Rectangle) getChildren().get(i)).setHeight(h);
 				}
 		});
 	}
@@ -244,12 +130,4 @@ public class Grid extends VBox{
 
 		return  (parameter >>> shifter) - 1;
 	}
-
-/*	private int getShifter(int i, int numberOfElements){
-
-		return (i == 0 || i == numberOfElements - 1)
-						  ? 5
-						  : 1 + (Math.min(i, numberOfElements - i) >>> 1);
-	}*/	// * disposable
-
 }
